4cafdec61bcd7af28846881f146ed46f6a5bc2f8
john_brochue <john.brochue@ghielectronics.com>
2013-06-18 10:44:11 -0400

    Added accelerometer, returns all 0's, needs investigation.

---
+++ CPP/Tests/FEZMedusa/Accelerometer/Accelerometer.ino
@@ -0,0 +1,31 @@
+#include <Gadgeteering.h>
+#include <FEZMedusa.h>
+#include <Accelerometer.h>
+#include <SPI.h>
+#include <SoftwareSerial.h>
+
+using namespace GHI;
+using namespace GHI::Interfaces;
+using namespace GHI::Mainboards;
+using namespace GHI::Modules;
+
+FEZMedusa board;
+Accelerometer* accel;
+
+void setup() {  
+  Serial.begin(9600);
+  accel = new Accelerometer(1);
+}
+
+void loop() {    
+  Accelerometer::Acceleration acc = accel->RequestMeasurement();
+
+  //Serial.print(acc.X);
+  //Serial.print(" ");  
+  //Serial.print(acc.Y);
+  //Serial.print(" ");  
+  //Serial.println(acc.Z);
+  
+  delay(100);
+}
+
