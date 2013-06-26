becdbd31c53633ddca008b600a701071e8ab1fb9
john_brochue <john.brochue@ghielectronics.com>
2013-06-26 16:20:55 -0400

    Added some destructors to interfaces.

--- CPP/Gadgeteering/Interfaces.hpp
+++ CPP/Gadgeteering/Interfaces.hpp
@@ -12,6 +12,7 @@ namespace GHI {
 			public:
 				DigitalOutput(Socket* socket, Socket::Pin pinNumber, bool initialState = false);
 				DigitalOutput(CPUPin pin, bool initialState = false);
+				~DigitalOutput();
 
 				void write(bool value);
 		};
@@ -22,6 +23,7 @@ namespace GHI {
 			public:
 				DigitalInput(Socket* socket, Socket::Pin pinNumber, ResistorMode resistorMode = ResistorModes::FLOATING);
 				DigitalInput(CPUPin pin, ResistorMode resistorMode = ResistorModes::FLOATING);
+				~DigitalInput();
 
 				bool read();
 				void setResistorMode(ResistorMode resistorMode);
@@ -35,6 +37,7 @@ namespace GHI {
 			public:
 				DigitalIO(Socket* socket, Socket::Pin pinNumber);
 				DigitalIO(CPUPin pin);
+				~DigitalIO();
 
 				void write(bool value);
 				bool read();
@@ -48,6 +51,7 @@ namespace GHI {
 			public:
 				AnalogInput(Socket* socket, Socket::Pin pinNumber);
 				AnalogInput(CPUPin pin);
+				~AnalogInput();
 				
 				double read();
 				double readProportion();
@@ -59,6 +63,7 @@ namespace GHI {
 			public:
 				AnalogOutput(Socket* socket, Socket::Pin pinNumber);
 				AnalogOutput(CPUPin pin);
+				~AnalogOutput();
 				
 				void write(double value);
 				void writeProportion(double value);
@@ -73,6 +78,7 @@ namespace GHI {
 			public:
 				PWMOutput(Socket* socket, Socket::Pin pinNumber);
 				PWMOutput(CPUPin pin);
+				~PWMOutput();
 
 				void set(double frequency, double dutyCycle);
 				void setFrequency(double frequency);
