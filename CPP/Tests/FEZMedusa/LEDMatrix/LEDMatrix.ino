670b9435bdfe160f09dc030d30d2bed5fb00a393
john_brochue <john.brochue@ghielectronics.com>
2013-06-27 11:33:13 -0400

    Actually added LEDMatrix tester.

---
+++ CPP/Tests/FEZMedusa/LEDMatrix/LEDMatrix.ino
@@ -0,0 +1,26 @@
+#include <Gadgeteering.h>
+#include <FEZMedusa.h>
+#include <SPI.h>
+#include <IO60P16.h>
+#include <LEDMatrix.h>
+
+using namespace GHI;
+using namespace GHI::Interfaces;
+using namespace GHI::Mainboards;
+using namespace GHI::Modules;
+
+FEZMedusa board;
+LEDMatrix* led;
+
+void setup() {
+  Serial.begin(9600);
+
+  led = new LEDMatrix(1);
+
+  unsigned char Letter_x[8] = { 0x0, 0x0, 0x42, 0x24, 0x18, 0x18, 0x24, 0x42 };
+  led->DrawBitmap(Letter_x);
+}
+
+void loop() {
+
+}
