744505b34bdeb17144be2b44594478ac3e981cad
john_brochue <john.brochue@ghielectronics.com>
2013-06-28 12:15:21 -0400

    Added RotaryH1 and PulseCount driver and testers.

---
+++ CPP/Tests/FEZMedusa/PulseCount/PulseCount.ino
@@ -0,0 +1,30 @@
+#include <Gadgeteering.h>
+#include <FEZMedusa.h>
+#include <SPI.h>
+#include <IO60P16.h>
+#include <PulseCount.h>
+
+using namespace GHI;
+using namespace GHI::Interfaces;
+using namespace GHI::Mainboards;
+using namespace GHI::Modules;
+
+FEZMedusa board;
+PulseCount* rotary;
+
+void setup() {
+  Serial.begin(9600);
+  rotary = new PulseCount(1);
+}
+
+void loop() {
+  Serial.print((int)rotary->ReadEncoders());
+  Serial.print(" ");
+  Serial.print((int)rotary->ReadDirection());
+  Serial.print(" ");
+  Serial.println((int)rotary->ReadStatusReg());
+    
+  delay(100);
+}
+
+
---
+++ CPP/Tests/FEZMedusa/RotaryH1/RotaryH1.ino
@@ -0,0 +1,30 @@
+#include <Gadgeteering.h>
+#include <FEZMedusa.h>
+#include <SPI.h>
+#include <IO60P16.h>
+#include <RotaryH1.h>
+
+using namespace GHI;
+using namespace GHI::Interfaces;
+using namespace GHI::Mainboards;
+using namespace GHI::Modules;
+
+FEZMedusa board;
+RotaryH1* rotary;
+
+void setup() {
+  Serial.begin(9600);
+  rotary = new RotaryH1(1);
+}
+
+void loop() {
+  Serial.print((int)rotary->ReadEncoders());
+  Serial.print(" ");
+  Serial.print((int)rotary->ReadDirection());
+  Serial.print(" ");
+  Serial.println((int)rotary->ReadStatusReg());
+    
+  delay(100);
+}
+
+
