88ffa72eacc601267d34ba516d233101fc641e3e
John <john.brochue@ghielectronics.com>
2013-11-04 10:38:34 -0500

    Fixed a bug in ColorSense.

--- CPP/ColorSense/ColorSense.cpp
+++ CPP/ColorSense/ColorSense.cpp
@@ -5,7 +5,7 @@ Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-   http://www.apache.org/licenses/LICENSE-2.0
+http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
@@ -23,10 +23,10 @@ using namespace GHI::Interfaces;
 ColorSense::ColorSense(unsigned char socketNumber) {
 	Socket* socket = mainboard->getSocket(socketNumber);
 	socket->ensureTypeIsSupported(Socket::Types::X);
-	
-    this->LEDControl = new DigitalOutput(socket, Socket::Pins::Three, false);
 
-	I2CBus* bus = mainboard->getI2CBus(socket->pins[4], socket->pins[5]);
+	this->LEDControl = new DigitalOutput(socket, Socket::Pins::Three, false);
+
+	I2CBus* bus = mainboard->getI2CBus(socket->pins[5], socket->pins[4]);
 	this->softwareI2C = bus->getI2CDevice(ColorSense::COLOR_ADDRESS);
 
 
@@ -34,7 +34,7 @@ ColorSense::ColorSense(unsigned char socketNumber) {
 }
 
 ColorSense::~ColorSense() {
-    delete this->LEDControl;
+	delete this->LEDControl;
 	delete this->softwareI2C;
 }
 
@@ -44,40 +44,40 @@ void ColorSense::ToggleOnboardLED(bool LEDState) {
 }
 
 ColorSense::ColorChannels ColorSense::ReadColorChannels() {
-    ColorChannels returnData;
+	ColorChannels returnData;
 
-    unsigned char TransmitBuffer = 0x00;
+	unsigned char TransmitBuffer = 0x00;
 	unsigned char read;
 	unsigned int a, b;
 
-    TransmitBuffer = 0x90; // Send COMMAND to access Green Color Channel register for chip
+	TransmitBuffer = 0x90; // Send COMMAND to access Green Color Channel register for chip
 	softwareI2C->writeRead(&TransmitBuffer, 1, &read, 1, &a, &b);
-    returnData.Green = read;
-    TransmitBuffer = 0x91;
+	returnData.Green = read;
+	TransmitBuffer = 0x91;
 	softwareI2C->writeRead(&TransmitBuffer, 1, &read, 1, &a, &b);
-    returnData.Green |= (unsigned int)(read) << 8;
-    //returnData.Green = 256 * (uint)readWord(TransmitBuffer)[0] + returnData.Green;
+	returnData.Green |= (unsigned int)(read) << 8;
+	//returnData.Green = 256 * (uint)readWord(TransmitBuffer)[0] + returnData.Green;
 
-    TransmitBuffer = 0x92; // Send COMMAND to access Red Color Channel register for chip
+	TransmitBuffer = 0x92; // Send COMMAND to access Red Color Channel register for chip
 	softwareI2C->writeRead(&TransmitBuffer, 1, &read, 1, &a, &b);
-    returnData.Red = read;
-    TransmitBuffer = 0x93;
+	returnData.Red = read;
+	TransmitBuffer = 0x93;
 	softwareI2C->writeRead(&TransmitBuffer, 1, &read, 1, &a, &b);
-    returnData.Red |= (unsigned int)(read) << 8;
+	returnData.Red |= (unsigned int)(read) << 8;
 
-    TransmitBuffer = 0x94; // Send COMMAND to access Blue Color Channel register for chip
+	TransmitBuffer = 0x94; // Send COMMAND to access Blue Color Channel register for chip
 	softwareI2C->writeRead(&TransmitBuffer, 1, &read, 1, &a, &b);
-    returnData.Blue = read;
-    TransmitBuffer = 0x95;
+	returnData.Blue = read;
+	TransmitBuffer = 0x95;
 	softwareI2C->writeRead(&TransmitBuffer, 1, &read, 1, &a, &b);
-    returnData.Blue |= (unsigned int)(read) << 8;
+	returnData.Blue |= (unsigned int)(read) << 8;
 
-    TransmitBuffer = 0x96; // Send COMMAND to access Clear Channel register for chip
+	TransmitBuffer = 0x96; // Send COMMAND to access Clear Channel register for chip
 	softwareI2C->writeRead(&TransmitBuffer, 1, &read, 1, &a, &b);
-    returnData.Clear = read;
-    TransmitBuffer = 0x97;
+	returnData.Clear = read;
+	TransmitBuffer = 0x97;
 	softwareI2C->writeRead(&TransmitBuffer, 1, &read, 1, &a, &b);
-    returnData.Clear |= (unsigned int)(read) << 8;
+	returnData.Clear |= (unsigned int)(read) << 8;
 
-    return returnData;
+	return returnData;
 }
