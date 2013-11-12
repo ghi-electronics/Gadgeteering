ac878a8d094bb32d93d3809eb1fabe98f0f7190b
John <john.brochue@ghielectronics.com>
2013-11-12 16:43:36 -0500

    Fixed wrong company name in licenses.

--- Mainboards/FEZLynxS4/s_I2CBus.cpp
+++ Mainboards/FEZLynxS4/s_I2CBus.cpp
@@ -1,5 +1,5 @@
 ﻿/*
-Copyright 2013 Gadgeteering Electronics LLC
+Copyright 2013 GHI Electronics LLC
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
