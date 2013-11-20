110ba5e56ae30aa08f4a99994a8f4af2cc121bc1
John <john.brochue@ghielectronics.com>
2013-11-20 11:53:23 -0500

    Updated an include.

--- Modules/MulticolorLED.cpp
+++ Modules/MulticolorLED.cpp
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-#include "MultiColorLED.h"
+#include "MulticolorLED.h"
 
 using namespace gadgeteering;
 using namespace gadgeteering::modules;
