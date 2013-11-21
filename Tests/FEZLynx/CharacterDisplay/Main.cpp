197927334614ee90fe76bc70b6b775d277d9cc8b
James Dudeck <james.dudeck@ghielectronics.com>
2013-11-21 15:25:58 -0500

    Added Character Display and Current testers

--- Tests/FEZLynx/CharacterDisplay/Main.cpp
+++ Tests/FEZLynx/CharacterDisplay/Main.cpp
@@ -3,7 +3,7 @@
 #include <Gadgeteering.h>
 
 #include <Mainboards/FEZLynxS4.h>
-#include <Modules/REPLACME.h>
+#include <Modules/CharacterDisplay.h>
 
 using namespace std;
 using namespace gadgeteering;
@@ -13,6 +13,20 @@ using namespace gadgeteering::modules;
 int main(int argc, char** argv)
 {
 	fez_lynx_s4 board;
+	character_display disp(1);
+
+	disp.set_backlight(true);
+
+	disp.clear();
+	disp.cursor_home();
+	disp.print("Sample Output");
+	system::sleep(500);
+
+	disp.set_cursor(0x00,0x01);
+	disp.print("i");
+	system::sleep(500);
+
+	disp.set_backlight(false);
 
 	return 0;
 }
--- Tests/FEZLynx/Current/Main.cpp
+++ Tests/FEZLynx/Current/Main.cpp
@@ -3,9 +3,10 @@
 #include <Gadgeteering.h>
 
 #include <Mainboards/FEZLynxS4.h>
-#include <Modules/REPLACME.h>
+#include <Modules/Current.h>
 
-using namespace std;
+#include <iostream>
+
 using namespace gadgeteering;
 using namespace gadgeteering::mainboards;
 using namespace gadgeteering::modules;
@@ -13,6 +14,15 @@ using namespace gadgeteering::modules;
 int main(int argc, char** argv)
 {
 	fez_lynx_s4 board;
+	current sensor(1);
+
+	while(true)
+	{
+		std::cout << "Peak Current: " << sensor.get_current_peak() << std::endl;
+		std::cout << "Current consumption: " << sensor.get_current_reading() << std::endl;
+
+		system::sleep(100);
+	}
 
 	return 0;
 }
