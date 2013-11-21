1b17d5bedb3bce79c4b4149be5d4667b9cd09f22
John <john.brochue@ghielectronics.com>
2013-11-21 14:17:38 -0500

    Added Button and LED7R testers.

--- Tests/FEZLynx/Button/Main.cpp
+++ Tests/FEZLynx/Button/Main.cpp
@@ -1,7 +1,11 @@
+#include <iostream>
+
 #include <Gadgeteering.h>
 
 #include <Mainboards/FEZLynxS4.h>
+#include <Modules/Button.h>
 
+using namespace std;
 using namespace gadgeteering;
 using namespace gadgeteering::mainboards;
 using namespace gadgeteering::modules;
@@ -9,6 +13,33 @@ using namespace gadgeteering::modules;
 int main(int argc, char** argv)
 {
 	fez_lynx_s4 board;
+	button b(1);
+
+	b.turn_led_on();
+	system::sleep(500);
+	if (b.is_led_on() == false)
+		cout << "FAIL" << endl;
+
+	b.turn_led_off();
+	system::sleep(500);
+	if (b.is_led_on() == true)
+		cout << "FAIL" << endl;
+
+	b.toggle_led();
+	system::sleep(500);
+	if (b.is_led_on() == false)
+		cout << "FAIL" << endl;
+
+	b.toggle_led();
+	system::sleep(500);
+	if (b.is_led_on() == true)
+		cout << "FAIL" << endl;
+
+	while (true)
+	{
+		b.set_led(b.is_pressed());
+		system::sleep(50);
+	}
 
 	return 0;
 }
--- Tests/FEZLynx/LED7R/Main.cpp
+++ Tests/FEZLynx/LED7R/Main.cpp
@@ -1,6 +1,7 @@
 #include <Gadgeteering.h>
 
 #include <Mainboards/FEZLynxS4.h>
+#include <Modules/LED7R.h>
 
 using namespace gadgeteering;
 using namespace gadgeteering::mainboards;
@@ -9,6 +10,37 @@ using namespace gadgeteering::modules;
 int main(int argc, char** argv)
 {
 	fez_lynx_s4 board;
+	led7r led(1);
+
+	led.animate(250, true, true, false);
+
+	led.turn_all_off();
+
+	for (int i = 0; i < 7; i++)
+	{
+		led.turn_on_led(i);
+		system::sleep(50);
+	}
+
+	for (int i = 0; i < 7; i++)
+	{
+		led.turn_off_led(i);
+		system::sleep(50);
+	}
+
+	system::sleep(1000);
+	led.set(5, true);
+	system::sleep(1000);
+	led.set(5, false);
+	system::sleep(1000);
+
+	bool next = false;
+	while (true)
+	{
+		next ? led.turn_all_off() : led.turn_all_on();
+		next = !next;
+		system::sleep(50);
+	}
 
 	return 0;
 }
